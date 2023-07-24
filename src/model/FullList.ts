784a11e94b31740cdcb94539917034fcf902b27e
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2023-07-24 15:27:07 +0330

    create a FUllList class

---
+++ src/model/FullList.ts
@@ -0,0 +1,39 @@
+import ListItem from './ListItem';
+
+interface List {
+    list: ListItem[];
+    load(): void;
+    save(): void;
+    clearList(): void;
+    addItem(itemObj: ListItem): void;
+    removeItem(id: string): void;
+}
+
+export default class FullList implements List {
+    static instance: FullList = new FullList();
+    private constructor(private _list: ListItem[] = []) {}
+    load(): void {
+        const storedList: string | null = localStorage.getItem('myList');
+        if (typeof storedList !== 'string') return;
+        const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+            JSON.parse(storedList);
+    }
+    save(): void {
+        localStorage.setItem('myList', JSON.stringify(this._list));
+    }
+    get list(): ListItem[] {
+        return this._list;
+    }
+    clearList(): void {
+        this._list = [];
+        this.save();
+    }
+    addItem(itemObj: ListItem): void {
+        this._list.push(itemObj);
+        this.save();
+    }
+    removeItem(id: string): void {
+        this._list = this._list.filter(item => item.id !== id);
+        this.save();
+    }
+}
